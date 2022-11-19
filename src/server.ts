587c7d9af6577b47ff6bff7239e89c508644f77c
TecSistem <69233035+luksdev@users.noreply.github.com>
2022-11-19 05:15:53 -0300

    revert: remove express-async-errors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,3 @@
-// import "express-async-errors";
 import express from "express";
 
 import { userRouter } from "./routes/user.routes";
@@ -9,7 +8,6 @@ const app = express();
 app.use(express.json());
 
 app.use("/", userRouter);
-
 app.use("/", itemRouter);
 
 app.listen(3333, () => console.log("Server is running in port 3333!"));
